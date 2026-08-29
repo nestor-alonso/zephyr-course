1d15912512aa5f06551d9d425557fef5798d48cb
Nestor Alonso Torres <nestor.linux@gmail.com>
2026-08-29 18:12:45 +0200

    Added overlay for LED0 and APP_HEARTBEAT_PERIOD_MS for sleep duration

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,8 +6,12 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
+#define APP_LED DT_ALIAS(app_led)
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+// From Module 3
+//static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+// From Module 4
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(APP_LED, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -24,7 +28,10 @@ int main(void)
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_LED_BLINK_SLEEP_TIME_MS);
+	// From Module 3
+        //k_msleep(CONFIG_LED_BLINK_SLEEP_TIME_MS);
+        // From Module 4
+	k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
